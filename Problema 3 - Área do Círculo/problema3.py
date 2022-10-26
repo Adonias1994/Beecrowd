3d79f248af83d0b013160a261fe98617b0194e6d
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-10-25 23:45:37 -0300

    Add files via upload
    Inacabado. Vou continuar outro dia, pois estou com sono e falta ajustar algumas coisas no código.

---
+++ Problema 3 - Área do Círculo/problema3.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# A fórmula da área do círculo é area = n.raiox2, onde n = 3.14159
+n = 3.14159
+raio = float(input())
+area = n * (raio * 2)
+print('A=', area)
+#Continuarei outro dia...
